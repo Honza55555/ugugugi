afa189e72fd604ac68d6d26c2595f675394c360f
Honza55555 <jan.durcak29@gmail.com>
2025-06-16 21:27:47 +0200

    Create imghdr.py

---
+++ imghdr.py
@@ -0,0 +1,6 @@
+# imghdr.py — stub aby telegram import prošel na Python 3.13+
+
+def what(file, h=None):
+    # Telegram balík očekává tuto funkci,
+    # my ale neanalyzujeme obrázky – vracíme None.
+    return None
